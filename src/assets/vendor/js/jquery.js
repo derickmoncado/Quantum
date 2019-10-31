d70b7f4911b2d0285e3740383bb7a23a400d98b9
Derick Moncado <derickmoncado@gmail.com>
2019-10-30 21:04:32 -0700

    Some more cleanup.

--- src/assets/vendor/js/jquery.js
+++ src/assets/vendor/js/jquery.js
@@ -73,11 +73,11 @@ var support = {};
 
 var isFunction = function isFunction( obj ) {
 
-      // Support: Chrome <=57, Firefox <=52
-      // In some browsers, typeof returns "function" for HTML <object> elements
-      // (i.e., `typeof document.createElement( "object" ) === "function"`).
-      // We don't want to classify *any* DOM node as a function.
-      return typeof obj === "function" && typeof obj.nodeType !== "number";
+	  // Support: Chrome <=57, Firefox <=52
+	  // In some browsers, typeof returns "function" for HTML <object> elements
+	  // (i.e., `typeof document.createElement( "object" ) === "function"`).
+	  // We don't want to classify *any* DOM node as a function.
+	  return typeof obj === "function" && typeof obj.nodeType !== "number";
   };
 
 
