8c68f076f8646dfdfec856a392b7f68058017032
Derick Moncado <derick.moncado@blueriver.com>
2019-11-01 15:20:27 -0700

    Quick change to jQuery

--- dist/assets/js/custom.js
+++ dist/assets/js/custom.js
@@ -8,12 +8,7 @@ $(document).ready(function () {
   //endRemoveIf(production)
   // For appending the 'active' class
 
-  $('.navbar-nav > li > a[href="' + pathname + '"]').parent().addClass('active'); // For appending the 'active' class to dropdowns (the children)
-
-  $('.navbar-nav > li > a .dropdown-menu a').click(function () {
-    $(this).parent().removeClass("active");
-    $('.dropdown-item').addClass("XXXTESTINGXXX");
-  });
+  $('.navbar-nav > li > a[href="' + pathname + '"]').parent().addClass('active');
   $('.home-slick-carousel').slick({
     dots: true,
     mobileFirst: true,
