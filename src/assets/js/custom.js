139c4df64a9dfd22451714d8cd00216a8fe07038
Derick Moncado <derickmoncado@gmail.com>
2021-09-05 21:51:47 -0500

    Working on nav dropdown fix

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -4,11 +4,8 @@ let pathname = window.location.pathname; // Gets URL path and stores in pathname
 
 $(document).ready(function() {
 	console.log('document ready!');
-	//removeIf(production)
-		// Be careful putting code here!
-	//endRemoveIf(production)
 
-	// For appending the 'active' class
+	// For appending the 'active' class to nav
 	$('.navbar-nav > li > a[href="'+pathname+'"]').parent().addClass('active');
 	$('.dropdown-menu > li > a[href="'+pathname+'"]').addClass('active');
 
